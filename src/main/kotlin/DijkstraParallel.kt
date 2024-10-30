4280deabd1f8d8d3fe582bcebcfa7934ac2d7fb0
Sofia <indecenet@gmail.com>
2024-10-30 23:01:09 +0300

    Edited README and DijkstraParallel.

--- src/main/kotlin/DijkstraParallel.kt
+++ src/main/kotlin/DijkstraParallel.kt
@@ -3,7 +3,6 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.sync.withLock
-import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.PriorityBlockingQueue
 
 class DijkstraParallel {
@@ -13,28 +12,27 @@ class DijkstraParallel {
 
         val priorityQueue = PriorityBlockingQueue<Pair<Int, Int>>(10,compareBy { it.second })
         priorityQueue.add(Pair(start, 0))
-        val visited = ConcurrentHashMap<Int, Boolean>()
 
         coroutineScope {
             while (priorityQueue.isNotEmpty()) {
+
                 val tasks = mutableListOf<Deferred<Unit>>()
-                repeat(priorityQueue.size) {
-                    val (currentVertex, currentDistance) = priorityQueue.poll() ?: return@coroutineScope
 
-                    if (visited.putIfAbsent(currentVertex, true) != null) return@coroutineScope
+                val (currentVertex, currentDistance) = priorityQueue.poll()
 
-                    tasks.add(async {
-                        for (edge in graph.adjList[currentVertex]) {
-                            val newDistance = currentDistance + edge.weight
-                            graph.mutex.withLock {
-                                if (newDistance < distances[edge.destination]) {
-                                    distances[edge.destination] = newDistance
-                                    priorityQueue.add(Pair(edge.destination, newDistance))
-                                }
+                if (currentDistance > distances[currentVertex]) continue
+
+                tasks.add(async {
+                    for (edge in graph.adjList[currentVertex]) {
+                        val newDistance = currentDistance + edge.weight
+                        graph.mutex.withLock {
+                            if (newDistance < distances[edge.destination]) {
+                                distances[edge.destination] = newDistance
+                                priorityQueue.add(Pair(edge.destination, newDistance))
                             }
                         }
-                    })
-                }
+                    }
+                })
                 tasks.awaitAll()
             }
         }
